012396e5a1866953935f7adc8569dc7975fd0ba3
DataStu <smkean@proton.me>
2025-01-21 13:34:45 +1100

    Storyline Example

--- Storyline/story_content/678vopvxa8M_transcripts.js
+++
@@ -1,5 +0,0 @@
-
-(function() {
-    const data = {"transcripts":[{"name":"captions","cues":[{"start":2310,"text":"Ok golfers. Let's talk about the swing."},{"start":4688,"text":"We're going to take our setup. We're"},{"start":7067,"text":"gonna make sure we make a back swing"},{"start":9785,"text":"back. We're going to follow through,"},{"start":11823,"text":"gently brush the turf and do a nice"},{"start":14541,"text":"follow through towards the target."},{"start":25350,"text":"And that's our swing."}]}]};
-    window.globalLoadJsAsset('story_content/678vopvxa8M_transcripts.js', JSON.stringify(data));
-})();
--- Storyline/story_content/6erR02rmvBS_transcripts.js
+++
@@ -1,5 +0,0 @@
-
-(function() {
-    const data = {"transcripts":[{"name":"captions","cues":[{"start":2630,"text":"Alright golfers, let's talk about the"},{"start":4241,"text":"grip. First thing we need to do is make"},{"start":6658,"text":"sure our left hand sits on top, then our"},{"start":9074,"text":"right touching and sitting underneath. We"},{"start":10685,"text":"want both thumbs running down the front"},{"start":12565,"text":"of the grip. From"},{"start":15190,"text":"there. We're ready to go."},{"start":22700,"text":"And that's the grip."}]}]};
-    window.globalLoadJsAsset('story_content/6erR02rmvBS_transcripts.js', JSON.stringify(data));
-})();
